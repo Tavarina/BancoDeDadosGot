ccb7e87a6826e3f268acaf8d6ebf054bf285f296
Moniquesouzas <monique.souzas@gmail.com>
2021-11-25 12:55:37 -0300

    Atualização dos dados de pesquisa

---
+++ Tabela_respostas/queries_consulta_avliacao/EP mais bem avaliado.sql
@@ -0,0 +1,14 @@
+#QUAL O EPISÓDIO MAIS BEM AVALIADO?
+
+SELECT MAX(rating),Season, Episode, Title, Release_date, Votes FROM got_episodes_v4;
+
+
+
+
+
+
+
+
+
+
+
---
+++ Tabela_respostas/queries_consulta_avliacao/media_avaliacao_temp4.sql
@@ -0,0 +1,20 @@
+use projeto_modulo3;
+
+select * from got_episodes_v4;
+#QUAL A MÉDIA DE AVALIAÇÃO DA TEMPORADA 4?
+select avg(Rating), Season from got_episodes_v4 where Season = 4;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
